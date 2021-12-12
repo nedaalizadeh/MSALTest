7760a73b2961a71254664c8e8718a7e5578e5583
yasin <yasin.b@intuitiverobotics.com.au>
2021-12-13 00:52:31 +1100

    first

---
+++ app/src/main/java/net/intuitiverobotics/androidapp/MainActivity.java
@@ -0,0 +1,197 @@
+// Copyright (c) Microsoft Corporation.
+// All rights reserved.
+//
+// This code is licensed under the MIT License.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files(the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions :
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package net.intuitiverobotics.androidapp;
+
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.view.GravityCompat;
+
+import android.util.Base64;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+
+import com.google.android.material.navigation.NavigationView;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener,
+        OnFragmentInteractionListener{
+
+    enum AppFragment {
+        SingleAccount,
+        MultipleAccount,
+        B2C
+    }
+
+    private AppFragment mCurrentFragment;
+
+    private ConstraintLayout mContentMain;
+
+    PackageInfo info;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+        try {
+            info = getPackageManager().getPackageInfo("net.intuitiverobotics.androidapp", PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md;
+                md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                String hash = new String(Base64.encode(md.digest(), 0));
+                //String hash = new String(Base64.encodeBytes(md.digest()));
+                Log.e("LOG","hash key-->"+ hash);
+            }
+        } catch (PackageManager.NameNotFoundException e1) {
+            Log.e("LOG", "/name not found/->" + e1.toString());
+        } catch (NoSuchAlgorithmException e) {
+            Log.e("LOG", "/no such an algorithm/->" + e.toString());
+        } catch (Exception e) {
+            Log.e("LOG", "/exception/->" + e.toString());
+        }
+
+        mContentMain = findViewById(R.id.content_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
+
+        //Set default fragment
+        navigationView.setCheckedItem(R.id.nav_single_account);
+        setCurrentFragment(AppFragment.SingleAccount);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(final MenuItem item) {
+        final DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        drawer.addDrawerListener(new DrawerLayout.DrawerListener() {
+            @Override
+            public void onDrawerSlide(@NonNull View drawerView, float slideOffset) { }
+
+            @Override
+            public void onDrawerOpened(@NonNull View drawerView) { }
+
+            @Override
+            public void onDrawerClosed(@NonNull View drawerView) {
+                // Handle navigation view item clicks here.
+                int id = item.getItemId();
+
+                if (id == R.id.nav_single_account) {
+                    setCurrentFragment(AppFragment.SingleAccount);
+                }
+
+                if (id == R.id.nav_multiple_account) {
+                    setCurrentFragment(AppFragment.MultipleAccount);
+                }
+
+                if (id == R.id.nav_b2c) {
+                    setCurrentFragment(AppFragment.B2C);
+                }
+
+                drawer.removeDrawerListener(this);
+            }
+
+            @Override
+            public void onDrawerStateChanged(int newState) { }
+        });
+
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    private void setCurrentFragment(final AppFragment newFragment){
+        if (newFragment == mCurrentFragment) {
+            return;
+        }
+
+        mCurrentFragment = newFragment;
+        setHeaderString(mCurrentFragment);
+        displayFragment(mCurrentFragment);
+    }
+
+    private void setHeaderString(final AppFragment fragment){
+        switch (fragment) {
+            case SingleAccount:
+                getSupportActionBar().setTitle("Single Account Mode");
+                return;
+
+            case MultipleAccount:
+                getSupportActionBar().setTitle("Multiple Account Mode");
+                return;
+
+            case B2C:
+                getSupportActionBar().setTitle("B2C Mode");
+                return;
+        }
+    }
+
+    private void displayFragment(final AppFragment fragment){
+        switch (fragment) {
+            case SingleAccount:
+                attachFragment(new SingleAccountModeFragment());
+                return;
+
+            case MultipleAccount:
+                attachFragment(new MultipleAccountModeFragment());
+                return;
+
+            case B2C:
+                attachFragment(new B2CModeFragment());
+                return;
+        }
+    }
+
+    private void attachFragment(final Fragment fragment) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .setTransitionStyle(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                .replace(mContentMain.getId(),fragment)
+                .commit();
+    }
+}
